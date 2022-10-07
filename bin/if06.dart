3cae3cc0c0d6f4aef32f427303a278c46bba2319
lifestealer001 <abrormurtazaqulov@gmail.com>
2022-10-07 16:32:20 +0500

    done

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,23 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(int a){
+  int x,y,z,t;
+  x = a~/1000;
+  y = a~/100%10;
+  z = a%100~/10;
+  t = a%10; 
+  if(x>y && x>z && x>t){
+    return 4;
+  }  if(y>x && y>z && y>t){
+    return 3;
+  }  if(z>x && z>y && z>t){
+    return 2;
+  }  else{
+    return 1;
+  }
+  return a;
+}
 void main() {
+    print(func(6158));
 }
